cad6fe405bf4bd4d0e7f4795f3291fd00d5da073
Nicholasstiv1 <nicholas.guerino@gmail.com>
2025-10-07 00:58:41 -0400

    cors & helmet

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './src/routes/home.routes.js';
 import userRoutes from './src/routes/user.routes.js';
@@ -18,6 +20,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const uploadPath = resolve(__dirname, 'uploads');
 
+const whiteList = [
+  'http://localhost:3000'
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else{
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
 
 class App {
   constructor() {
@@ -27,6 +42,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(uploadPath));
